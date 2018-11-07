e5a256801725ea67487be6c0af72eb78edfd70e9
rogercodes1 <rogerperez.us@gmail.com>
2018-11-07 12:28:15 -0800

    added product.model.ts to client/app/shared foler

--- src/client/app/app-routing.module.ts
+++ src/client/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ContactListComponent } from './contact-list/contact-list.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'contact',
+    pathMatch: 'full'
+  },
+  {
+    path: 'contacts',
+    component: ContactListComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/server/index.js
+++ src/server/index.js
@@ -1,11 +1,37 @@
 const express = require('express');
 const app = express();
-
 const MongoClient = require('mongodb').MongoClient;
+const bodyParser = require('body-parser');
 
 require('dotenv').config();
 
+app.use(bodyParser.json())
+
 let database;
+
+app.get('/api/contacts', (req, res) => {
+  const contactsCollection = database.collection('contacts');
+
+  contactsCollection.find({}).toArray((err, docs) => {
+    return res.json(docs);
+  });
+});
+
+app.post('/api/contacts', (req,res) => {
+  const user = req.body;
+
+  const contactsCollection = database.collection('contacts');
+
+  contactsCollection.insertOne(user,(err,r) => {
+    if (err){
+      return res.status(500).json({ error: "Error inserting new record."})
+    }
+
+    const newRecord = r.ops[0];
+
+    return res.status(201).json(newRecord)
+  })
+})
 
 MongoClient.connect(process.env.DB_CONN, (err, db) => {
 
@@ -16,12 +42,4 @@ MongoClient.connect(process.env.DB_CONN, (err, db) => {
     console.log('listening on port 3000..');
   });
 
-});
-
-app.get('/contacts', (req, res) => {
-  const contactsCollection = database.collection('contacts');
-
-  contactsCollection.find({}).toArray((err, docs) => {
-    return res.json(docs);
-  });
 });
