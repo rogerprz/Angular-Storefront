f8c1fffe4f89ad9298d10f25a93116d6f989ea4a
rogercodes1 <rogerperez.us@gmail.com>
2018-11-13 15:15:11 -0800

    updated object creation data.

--- src/client/app/add-product/add-product.component.ts
+++ src/client/app/add-product/add-product.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import * as $ from 'jquery';
+import { Http, Response, RequestOptions, Headers } from '@angular/http';
+import { NgForm } from '@angular/forms';
+import { Product } from '../shared/contact.model';
+
 
 @Component({
   selector: 'app-add-product',
@@ -8,29 +12,30 @@ import * as $ from 'jquery';
 })
 export class AddProductComponent implements OnInit {
 
-  constructor() { }
+  loading: Boolean false; //component ui state
+  newProduct: Product;
+
+  constructor(public http:Http) { }
 
   ngOnInit() {
-    // $(document).ready(function(){
-    //
-    //   $('.select')
-    //   .dropdown({
-    //     values: [
-    //       {
-    //         name: 'Physical',
-    //         value: 'physical'
-    //       },
-    //       {
-    //         name     : 'Digital',
-    //         value    : 'digital',
-    //       },
-    //       {
-    //         name     : 'Mixed',
-    //         value    : 'mixed',
-    //       }
-    //     ]
-    //   })
-    // }
+
+  }
+  onSubmit(form: NgForm){
+    this.loading = true;
+
+    const formValues = Object.assign({}, form.value){
+      name: `${formValues.name}`,
+      quantity_available:`${formValues.quantityAvailable}`,
+      minimum_quantity:`${formValues.minimumQuantity}`,
+      editable_quantity:`${formValues.editableQuantity}`,
+      format:`${formValues.format}`,
+      unit_price:`${formValues.unitPrice}`,
+      discount_price:`${formValues.discountPrice}`,
+
+
+
+    }
+
   }
 
 }
