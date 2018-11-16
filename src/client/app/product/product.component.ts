2adc197ae80bc29e6dba0735b86fac3c2d187dee
rogercodes1 <rogerperez.us@gmail.com>
2018-11-16 12:29:32 -0800

    fixed layout display and fixed to 2 decimal places

--- src/client/app/product/product.component.ts
+++ src/client/app/product/product.component.ts
@@ -8,7 +8,7 @@ import { ApiService } from '../shared/api.service'
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
-  styleUrls: ['./product.component.css']
+  styleUrls: ['./product.component.css'],
 })
 export class ProductComponent implements OnInit {
 
